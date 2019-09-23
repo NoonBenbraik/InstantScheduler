0908eb15251223123bdd5cf2fb212da1bae5199f
Nader Benbraik <nader.benbraik@gmail.com>
2019-09-23 19:23:35 +0300

    EFW

---
+++ DAL/InstaContext.cs
@@ -0,0 +1,24 @@
+﻿using InstantScheduler.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantScheduler.DAL
+{
+    public class InstaContext : DbContext
+    {
+        public InstaContext() : base()
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<ScheduleModel> Schedules { get; set; }
+        public DbSet<SearchModel> Searches { get; set; }
+        public DbSet<TaskModel> Tasks { get; set; }
+
+    }
+}
